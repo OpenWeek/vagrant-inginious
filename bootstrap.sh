e005cb0c7b06f214ec0f109d26ad65b2df46b1d1
Maxmaw <max.mawait@gmail.com>
2019-07-01 00:26:27 +0200

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,13 @@
-#Inginious install
-apt-get install -y git mongodb mongodb-server gcc libtidy python3 python3-pip python3-devel zeromq-devel redhat-rpm-config docker xmlsec1-openssl python3-xmlsec
+curl -fsSL https://get.docker.com/ | sh #This will setup the Docker repo
+apt-get install -y git mongodb gcc tidy python3 python3-pip python3-dev libzmq-dev
+apt-get install -y libldap2-dev libsasl2-dev libssl-dev
+apt-get install -y libxmlsec1-dev libltdl-dev
 
-systemctl unmask mongodb
-systemctl start mongod
-systemctl enable mongod
+systemctl start mongodb
+systemctl enable mongodb
 systemctl start docker
 systemctl enable docker
 
 pip3 install --upgrade pip
-
-# for syllabus webapp
-/usr/local/bin/pip3 install flask
-# for rst syntax highlighting
-/usr/local/bin/pip3 install pygments
 
 /usr/local/bin/pip3 install --upgrade git+https://github.com/UCL-INGI/INGInious
